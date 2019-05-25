3ceb138f3f06470a267da2e2fadad31e30450e23
MatheusGavette <matheus.gvtt10@gmail.com>
2019-05-25 12:20:33 -0300

    Estrutura das classes

---
+++ Herança_exemplo2_Trabalhador/Entities/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Herança_exemplo2_Trabalhador.Entities
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        public double ValuePerHour { get; set; }
+
+        public Employee (string name, int hours, double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePerHour;
+        }
+
+        public virtual double Payment()
+        {
+            return ValuePerHour * Hours;
+        }
+    }
+}
---
+++ Herança_exemplo2_Trabalhador/Entities/OutSourcedEmployee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Herança_exemplo2_Trabalhador.Entities
+{
+    class OutSourcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutSourcedEmployee (string name, int hours, double valuePerHours, double additionalCharge) 
+            : base (name, hours, valuePerHours)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + (AdditionalCharge * 1.10);
+        }
+    }
+}
